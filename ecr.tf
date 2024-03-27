3c077348710f22d8f4685008d3c108936a6eaa26
Tomlord88 <tom.isokpehi@gmail.com>
2024-03-26 21:49:28 -0500

    Add files via upload

---
+++ ecr.tf
@@ -0,0 +1,33 @@
+resource "aws_ecr_repository" "terra_project" {
+  name                 = "terra-project" # Name of the repository
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository_policy" "terra_project_policy" {
+  repository = aws_ecr_repository.terra_project.name
+
+  policy = jsonencode({
+    Version = "2008-10-17",
+    Statement = [
+      {
+        Sid = "AllowPushPull",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::767397734560:root" # Replace YOUR_AWS_ACCOUNT_ID with your actual AWS account ID
+        },
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
+}
--- eks_cluster.tf
+++ eks_cluster.tf
@@ -1,9 +1,15 @@
 resource "aws_eks_cluster" "tf_eks_cluster" {
-    name = "tf_eks_cluster"
-    role_arn = aws_iam_role.tf_eks_cluster.arn
-  
-  vpc_config {
-    subnet_ids = [aws_subnet.tfsubnet1.id,aws_subnet.tfsubnet2.id]
-  }
+  name     = "tf_eks_cluster"
+  role_arn = aws_iam_role.tf_eks_cluster.arn
 
+  vpc_config {
+    subnet_ids        = [aws_subnet.tfsubnet1.id, aws_subnet.tfsubnet2.id]
+  } 
+  # Example to enable logging (optional)
+  enabled_cluster_log_types = ["api", "audit"]
+
+  tags = {
+    Environment = "development"
+    Project     = "ProjectTerra"
+  }
 }
